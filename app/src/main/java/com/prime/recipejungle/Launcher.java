1110d25e655474227c0163bd6ef5f0ee2ea4a795
beyzaaltuntas <baltuntas@etu.edu.tr>
2019-07-20 00:57:00 +0300

    conflict fixed

--- app/src/main/java/com/prime/recipejungle/Launcher.java
+++ app/src/main/java/com/prime/recipejungle/Launcher.java
@@ -4,13 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-<<<<<<< HEAD
 import com.prime.recipejungle.activities.LoginActivity;
 import com.prime.recipejungle.activities.RegisterActivity;
-=======
 import com.prime.recipejungle.activities.CreateActivity;
-import com.prime.recipejungle.activities.HelloActivity;
->>>>>>> 32dd0034e69e4498a5858ad1612ebaab47267c4e
 import com.prime.redef.app.App;
 
 public class Launcher extends AppCompatActivity {
@@ -19,11 +15,8 @@ public class Launcher extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-<<<<<<< HEAD
         App.startActivity(this, LoginActivity.class, null);
-=======
         App.startActivity(this, CreateActivity.class, null);
->>>>>>> 32dd0034e69e4498a5858ad1612ebaab47267c4e
         finish();
     }
 }
