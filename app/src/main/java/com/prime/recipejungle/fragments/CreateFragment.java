a5cfaa019069d92273986cfdff71129c831e8e97
SemihCatal <catalsemih03@gmail.com>
2019-07-22 22:42:30 +0300

    Create fragment

--- app/src/main/java/com/prime/recipejungle/fragments/CreateFragment.java
+++ app/src/main/java/com/prime/recipejungle/fragments/CreateFragment.java
@@ -9,9 +9,23 @@ import android.widget.EditText;
 import androidx.annotation.NonNull;
 
 import com.prime.recipejungle.R;
+import com.prime.recipejungle.utils.Constants;
 import com.prime.redef.app.RedefActivity;
 import com.prime.redef.app.RedefFragment;
 import com.prime.redef.app.configs.ActivityConfig;
+import com.prime.redef.json.JObject;
+import com.prime.redef.json.Json;
+import com.prime.redef.network.ApiClient;
+import com.prime.redef.network.ApiRequest;
+import com.prime.redef.network.ApiRestHandler;
+import com.prime.redef.network.GetRequest;
+import com.prime.redef.network.Header;
+import com.prime.redef.network.PostRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class CreateFragment extends RedefFragment {
     private Button button;
@@ -26,6 +40,10 @@ public class CreateFragment extends RedefFragment {
     @Override
     public View onCreate(@NonNull Context context, @NonNull LayoutInflater inflater) {
         View content = inflater.inflate(R.layout.create_fragment, null);
+
+        final ApiClient client = new ApiClient(Constants.HOST);
+        final ApiRequest request = new PostRequest("/api/recipe/create");
+
         etTitle = content.findViewById(R.id.recipeTitle);
         etDescription = content.findViewById(R.id.recipeDescription);
         etPortion = content.findViewById(R.id.recipePortion);
@@ -36,16 +54,42 @@ public class CreateFragment extends RedefFragment {
         this.button = content.findViewById(R.id.button1);
 
 
-        button.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener()  {
             @Override
-            public void onClick(View v) {
-                String title = etTitle.getText().toString();
-                String description = etTitle.getText().toString();
-                int portion = Integer.parseInt(etPortion.getText().toString());
-                int prepareTime = Integer.parseInt(etPrepareTime.getText().toString());
-                String ingredients = etIngredients.getText().toString();
-                String tags = etTags.getText().toString();
-                String steps= etSteps.getText().toString();
+            public void onClick(View v)  {
+                final String title = etTitle.getText().toString();
+                final String description = etTitle.getText().toString();
+                final int portion = Integer.parseInt(etPortion.getText().toString());
+                final int prepareTime = Integer.parseInt(etPrepareTime.getText().toString());
+                final String ingredients = etIngredients.getText().toString();
+                final String tags = etTags.getText().toString();
+                final String steps= etSteps.getText().toString();
+
+                HashMap<String,Object> obj = new HashMap<>();
+                try {
+                    obj.put("Title", title);
+                    obj.put("Text", description);
+                    obj.put("Portion", portion);
+                    obj.put("PrepareTime", prepareTime);
+                    obj.put("Ingredients", ingredients);
+                    obj.put("Tags", tags);
+                    obj.put("Steps", steps);
+                    ((PostRequest) request).setJsonBody(Json.toJson(obj));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                client.execute(request, new ApiRestHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) throws Exception {
+                        super.onSuccess(statusCode, headers, responseBody);
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                        super.onFailure(statusCode, headers, responseBody, error);
+                    }
+                });
             }
         });
 
