cbda3756c9b557f832b8c6babf0b319b28fb744e
beyzaaltuntas <baltuntas@etu.edu.tr>
2019-07-23 00:26:06 +0300

    login and register implemented

--- app/src/main/java/com/prime/recipejungle/fragments/LoginFragment.java
+++ app/src/main/java/com/prime/recipejungle/fragments/LoginFragment.java
@@ -1,6 +1,7 @@
 package com.prime.recipejungle.fragments;
 
 import android.content.Context;
+import android.provider.SyncStateContract;
 import android.text.InputType;
 import android.text.method.LinkMovementMethod;
 import android.util.Log;
@@ -14,7 +15,10 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 
 import com.prime.recipejungle.R;
-import com.prime.recipejungle.utils.Constants;
+import com.prime.recipejungle.activities.LoginActivity;
+import com.prime.recipejungle.activities.RegisterActivity;
+import com.prime.recipejungle.utils.Global;
+import com.prime.redef.app.App;
 import com.prime.redef.app.RedefFragment;
 import com.prime.redef.json.JObject;
 import com.prime.redef.json.Json;
@@ -30,28 +34,23 @@ import java.util.HashMap;
 public class LoginFragment extends RedefFragment {
 
     private Button button;
+    private View content;
     private ApiClient apiClient;
     private EditText usertext;
     private EditText passtext;
-
-
+    private String username;
+    private String password;
+    private View register;
     @Override
     public View onCreate(@NonNull Context context, @NonNull LayoutInflater inflater) {
-        final View content = inflater.inflate(R.layout.login_fragment, null);
-        final EditText usertext = content.findViewById(R.id.editText1);
-        final EditText passtext = content.findViewById(R.id.editText2);
-
+        this.content = inflater.inflate(R.layout.login_fragment, null);
+        this.register = content.findViewById(R.id.view1);
         this.usertext = content.findViewById(R.id.editText1);
         this.passtext = content.findViewById(R.id.editText2);
-        //final TextView reg = content.findViewById(R.id.view1);
-
-        //passtext.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-        //((TextView) content.findViewById(R.id.view1)).setMovementMethod(LinkMovementMethod.getInstance());
-        // ((TextView) content.findViewById(R.id.view1)).setText(Html.fromHtml(content.getResources().getString(R.string.string_with_links)));
 
         passtext.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
 
-        apiClient = new ApiClient(Constants.HOST);
+        apiClient = new ApiClient(Global.HOST);
 
         this.button = content.findViewById(R.id.button1);
 
@@ -60,12 +59,23 @@ public class LoginFragment extends RedefFragment {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username = usertext.getText().toString();
-                String password = passtext.getText().toString();
+                if (usertext.getText()== null || passtext.getText() ==null )
+                    return;
+
+                username = usertext.getText().toString();
+                password = passtext.getText().toString();
 
                 buttonClicked();
             }
         });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                App.startActivity(getAndroidActivity(), RegisterActivity.class,null);
+            }
+        });
+
 
         return content;
     }
@@ -73,10 +83,9 @@ public class LoginFragment extends RedefFragment {
     private void buttonClicked() {
         PostRequest request = new PostRequest("/api/account/authenticate");
         HashMap<String, Object> body = new HashMap<>();
-        body.put("Username", "asdsadsd");
-        body.put("Password", "asdsadsd");
-        body.put("Email", "asdsadsd");
-        request.putHeader("Authorization", "sadsad");
+        body.put("Username", username);
+        body.put("Password", password);
+
         request.setJsonBody(Json.toJson(body));
 
         apiClient.execute(request, new ApiRestHandler() {
@@ -84,7 +93,9 @@ public class LoginFragment extends RedefFragment {
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                 String responseString = ObjectUtils.utf8String(responseBody);
                 String message = Json.fromJson(responseString, String.class);
-                Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+                Global.PROPERTIES.putString("Authentication:", message);
+                App.startActivity(getAndroidActivity(), RegisterActivity.class,null);
+
             }
 
             @Override
--- app/src/main/java/com/prime/recipejungle/fragments/RegisterFragment.java
+++ app/src/main/java/com/prime/recipejungle/fragments/RegisterFragment.java
@@ -9,36 +9,98 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
 import com.prime.recipejungle.R;
+import com.prime.recipejungle.activities.LoginActivity;
+import com.prime.recipejungle.activities.RegisterActivity;
+import com.prime.recipejungle.utils.Global;
+import com.prime.redef.app.App;
 import com.prime.redef.app.RedefFragment;
+import com.prime.redef.json.Json;
+import com.prime.redef.network.ApiClient;
+import com.prime.redef.network.ApiRestHandler;
+import com.prime.redef.network.Header;
+import com.prime.redef.network.PostRequest;
+import com.prime.redef.utils.ObjectUtils;
+
+import java.util.HashMap;
 
 public class RegisterFragment extends RedefFragment {
 
     private Button button;
-
+    private EditText usertext;
+    private EditText passtext;
+    private EditText emailtext;
+    private ApiClient apiClient;
+    private String username;
+    private String password;
+    private String email;
+    private View login;
     @Override
     public View onCreate(@NonNull Context context, @NonNull LayoutInflater inflater) {
+
         View content = inflater.inflate(R.layout.register_fragment, null);
-        final EditText usertext = content.findViewById(R.id.editText1);
-        final EditText passtext = content.findViewById(R.id.editText2);
-        final EditText emailtext = content.findViewById(R.id.editText3);
-        this.button = content.findViewById(R.id.button1);
+        usertext = content.findViewById(R.id.editText1);
+        passtext = content.findViewById(R.id.editText2);
+        emailtext = content.findViewById(R.id.editText3);
+        button = content.findViewById(R.id.button1);
+        login = content.findViewById(R.id.view1);
 
         passtext.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-
+        apiClient = new ApiClient(Global.HOST);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username = usertext.getText().toString();
-                String password = passtext.getText().toString();
-                String email = emailtext.getText().toString();
+                if (usertext.getText()== null || passtext.getText() ==null || emailtext.getText()==null)
+                    return;
+
+                password = passtext.getText().toString();
+                email = emailtext.getText().toString();
+                username =usertext.getText().toString();
+
+                buttonClicked();
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                App.startActivity(getAndroidActivity(), LoginActivity.class,null);
+
             }
         });
 
         return content;
+    }
+
+    private void buttonClicked(){
+        PostRequest request = new PostRequest("/api/account/register");
+        HashMap<String, Object> body = new HashMap<>();
+        body.put("Username", username);
+        body.put("Password", password);
+        body.put("Email",email);
+
+        request.setJsonBody(Json.toJson(body));
+
+        apiClient.execute(request, new ApiRestHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                String responseString = ObjectUtils.utf8String(responseBody);
+                String message = Json.fromJson(responseString, String.class);
+                App.startActivity(getAndroidActivity(), LoginActivity.class,null);
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                String responseString = ObjectUtils.utf8String(responseBody);
+                String message = Json.fromJson(responseString, String.class);
+                Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
