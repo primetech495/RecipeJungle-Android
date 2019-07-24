bea7f1f5efb9ca35ee56df1ea7800d8d79cbdf97
align-els <aligng1620@gmail.com>
2019-07-24 14:25:43 +0300

    ip changed

--- app/src/main/java/com/prime/recipejungle/utils/Global.java
+++ app/src/main/java/com/prime/recipejungle/utils/Global.java
@@ -3,6 +3,6 @@ package com.prime.recipejungle.utils;
 import com.prime.redef.io.Properties;
 
 public class Global {
-    public static final String HOST = "http://10.3.40.159:8080";
+    public static final String HOST = "http://172.104.157.202:5050";
     public static Properties PROPERTIES = new Properties("recipe", false);
 }
